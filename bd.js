cf303cbd141ef41f425a1be5dca847b4f1bde9d5
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-18 18:17:17 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -2,3 +2,36 @@ const usuario = {
     "email": "admin@email.com",
     "senha": "admin"
 };
+
+const dados = [
+    {
+        "id": 1,
+        "titulo": "Mobilidade Urbana em Belo Horizonte",
+        "descricao": "A mobilidade urbana em Belo Horizonte é cara.",
+        "conteudo": "A mobilidade urbana em Belo Horizonte foca na expansão do transporte coletivo e não motorizado, com projetos de ciclovias, corredores de BRT e aquisição de ônibus elétricos para o sistema MOVE.",
+        "categoria": "Transporte",
+        "autor": "Ana Maria Silva",
+        "data": "18/09/2025",
+        "imagem": "asset/mobilidade.png"
+    },
+    {
+        "id": 2,
+        "titulo": "Sexta Geração de Redes Móveis",
+        "descricao": "A sexta geração de redes móveis visa superar as limitações do 5G",
+        "conteudo": "Prevista para chegar comercialmente ao Brasil por volta de 2030, a rede unirá inteligência artificial, satélites e sensores para permitir aplicações como metaversos imersivos, cidades inteligentes, cirurgias remotas e até interfaces cérebro-computador.",
+        "categoria": "Tecnologia e Comunicação",
+        "autor": "Bruno Rafael de Souza",
+        "data": "17/09/2025",
+        "imagem": "asset/tecnologia-6g.png"
+    },
+    {
+        "id": 3,
+        "titulo": "Festival de Música",
+        "descricao": "4ª Edição do Festival de Música de BH",
+        "conteudo": "A 4ª edição do festival de música de BH acontecerá entre março e outubro de 2025, reunindo compositores e bandas de música de todo o Brasil.",
+        "categoria": "Cultura",
+        "autor": "Camila Maia Torres",
+        "data": "16/09/2025",
+        "imagem": "asset/festival.png"
+    }
+];
--- login.js
+++ login.js
@@ -16,5 +16,4 @@ function verificarCredenciais() {
     }
     else
         alert("E-mail informado incorretamente!");
-
 }
